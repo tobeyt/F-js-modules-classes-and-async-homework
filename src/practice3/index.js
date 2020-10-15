d1cac09a265bb0862c5aba6ddda37c858638336c
di.qin <di.qin@thoughtworks.com>
2020-10-15 19:04:19 +0800

    [qd]: finish practice3.

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,18 @@
-export const getPoetry
+export const getPoetry = async () => {
+  const URL = "https://v1.jinrishici.com/all.json";
+  try {
+    const response = await fetch(URL);
+    const jsonData = await response.json();
+    return [jsonData.origin, jsonData.author, jsonData.content];
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPoetry1 = () => {
+  const URL = "https://v1.jinrishici.com/all.json";
+  return fetch(URL)
+    .then((response) => response.json())
+    .then((jsonData) => [jsonData.origin, jsonData.author, jsonData.content])
+    .catch((error) => console.log(error));
+};
