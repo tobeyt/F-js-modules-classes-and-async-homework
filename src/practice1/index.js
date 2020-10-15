26149828e85ab3648eb07c4dbdc0d9ea59cbaa3d
di.qin <di.qin@thoughtworks.com>
2020-10-15 18:21:27 +0800

    [qd]: finish practice1.

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,7 @@
+import getURL from "./utils";
+import { ADDRESS, PATH, PORT, REGION } from "./constant";
 
+const url = getURL(ADDRESS, PORT, PATH);
+const region = REGION;
+
+export { url, region };
